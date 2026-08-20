4556e8b902d634b211b291b02a0e4275ce28db6f
Mas1985200 <mas1985dimond@gmail.com>
2026-08-20 21:10:03 +0330

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -24,7 +24,7 @@ export default function AppLayout() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex-1 min-w-0">
         <Outlet />
       </div>
     </div>
